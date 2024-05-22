9ab419a2c234bc091bb1c3aed3e2b9c00b124f10
LucasMontenegroM <lucasmontereira@gmail.com>
2024-05-21 21:34:18 -0300

    adicionado arquivos para melhorar organização.

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -4,6 +4,7 @@ const usuarioControlador = require("./controller/usuarioController");
 const resenhaControlador = require("./controller/resenhaController");
 const comentarioControlador = require("./controller/comentarioControlle");
 const favoritasControlador = require("./controller/favoritasController");
+const amigoControlador = require("./controller/amigoController");
 const auth = require("./middleware/auth");
 
 //CRUD usuario
@@ -14,11 +15,11 @@ router.delete("/usuario", auth, usuarioControlador.deletarUsuario);
 router.post("/login", usuarioControlador.login);
 
 //ROTAS PARA O BÔNUS DE AMIGOS
-router.get("/usuario/amigos", auth, usuarioControlador.verAmigos);
-router.get("/usuario/solicitacoes", auth, usuarioControlador.verSolicitacoes);
-router.post("/usuario/amigos", auth, usuarioControlador.adicionarAmigos);
-router.patch("/usuario/amigos", auth, usuarioControlador.aceitarSolicitacao);
-router.delete("/usuario/amigos", auth, usuarioControlador.deletarOuRejeitarAmigo);
+router.get("/usuario/amigos", auth, amigoControlador.verAmigos);
+router.get("/usuario/solicitacoes", auth, amigoControlador.verSolicitacoes);
+router.post("/usuario/amigos", auth, amigoControlador.adicionarAmigos);
+router.patch("/usuario/amigos", auth, amigoControlador.aceitarSolicitacao);
+router.delete("/usuario/amigos", auth, amigoControlador.deletarOuRejeitarAmigo);
 
 //CRUD resenha:
 router.post("/resenha", auth, resenhaControlador.createResenha);
