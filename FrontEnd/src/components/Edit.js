bf6f5fa936505ee1b730b3552a0647b507da5d26
Melissa <melissajqt18@gmail.com>
2024-05-23 10:29:55 -0300

    commit realizado com sucesso

---
+++ FrontEnd/src/components/Edit.js
@@ -0,0 +1,19 @@
+export default function Edit () {
+    return(
+        <button
+        style={{
+          backgroundColor: "#E6DCEE",
+          fontWeight: "bold",
+          borderRadius: "1.5rem",
+          marginLeft: 300,
+          fontSize: 17,
+          padding: "0.5rem",
+          paddingLeft: "0.8rem",
+          paddingRight: "0.8rem",
+        }}
+      >
+        Editar perfill
+        <br />
+      </button>
+    )
+}
